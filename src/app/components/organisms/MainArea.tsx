096acb92ccb173f5846542b631a9a7de2295f316
nishinohi <tomoya.nishi@azone-japan.co.jp>
2024-02-09 14:50:25 +0900

    これくらいスッキリ書ける

--- src/app/components/organisms/MainArea.tsx
+++ src/app/components/organisms/MainArea.tsx
@@ -10,12 +10,10 @@ export default function MainArea(props: Props) {
   const [prefectures, setPrefectures] = useState<PrefectureAndColor[]>(prefecturesData)
 
   const updatePrefecturesData = (code: number) => {
-    const newPrefecturesDatas = [...prefectures].map((prefecture) => {
-      if (code === prefecture.prefCode) {
-        return { ...prefecture, value: !prefecture.value }
-      }
-      return { ...prefecture }
-    })
+    const newPrefecturesDatas = [...prefectures].map((prefecture) => ({
+      ...prefecture,
+      value: code === prefecture.prefCode ? !prefecture.value : prefecture.value,
+    }))
     setPrefectures(newPrefecturesDatas)
   }
 
